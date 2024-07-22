d6491f70713503cf0eb99f103bdc200ba8c7cb2a
Оксана <106163937+GurinAOA@users.noreply.github.com>
2024-07-22 12:38:43 +0300

    delete slider

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
 "use strict"
-var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    effect: "fade",
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
